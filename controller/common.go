4eb999aad4263f1258e516564298fe1729242c79
BaiMeow <baishu2003@outlook.com>
2023-01-17 12:34:36 +0800

    feat: define models

--- controller/common.go
+++ controller/common.go
@@ -5,6 +5,29 @@ type Resp struct {
 	StatusMsg  string `json:"statusMsg,omitempty"`
 }
 
+type User struct {
+	Id            uint   `json:"id"`
+	Name          string `json:"name"`
+	FollowCount   uint   `json:"followCount"`
+	FollowerCount uint   `json:"followerCount"`
+	IsFollow      uint   `json:"isFollow"`
+}
+
 type Video struct {
-	// ...
+	Id            uint   `json:"id"`
+	Author        User   `json:"author"`
+	PlayUrl       string `json:"playUrl"`
+	CoverUrl      string `json:"coverUrl"`
+	FavoriteCount uint   `json:"favoriteCount"`
+	CommentCount  uint   `json:"commentCount"`
+	IsFavorite    uint   `json:"isFavorite"`
+	Title         string `json:"title"`
+}
+
+type Comment struct {
+	Id      uint   `json:"id"`
+	User    User   `json:"user"`
+	Content string `json:"content"`
+	//mm-dd
+	CreateDate string `json:"createDate"`
 }
