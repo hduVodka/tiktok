3f20180018e3e3c737b294050836871fccc6037c
jking412 <3250532948@qq.com>
2023-01-25 12:56:57 +0800

    fix: 修改service中的favorite

--- service/interactive/favorite.go
+++ service/interactive/favorite.go
@@ -3,6 +3,7 @@ package interactive
 import (
 	"errors"
 	"net/http"
+	"tiktok/db"
 	"tiktok/models"
 )
 
@@ -10,22 +11,22 @@ func FavoriteAction(favorite *models.Favorite, actionType int) (int, error) {
 	var exist bool
 	// 执行点赞或取消点赞操作
 	if actionType == 1 {
-		exist = favorite.IsFavorite()
+		exist = db.IsFavorite(favorite.UserID, favorite.VideoID)
 		if exist {
 			return http.StatusOK, nil
 		} else {
-			if err := favorite.InsertFavorite(); err != nil {
+			if err := db.InsertFavorite(favorite); err != nil {
 				return http.StatusInternalServerError, errors.New("点赞失败")
 			}
 			return http.StatusOK, nil
 		}
 	} else if actionType == 2 {
-		exist = favorite.IsFavorite()
+		exist = db.IsFavorite(favorite.UserID, favorite.VideoID)
 		if !exist {
 			return http.StatusOK, nil
 		}
 
-		if err := favorite.DeleteFavorite(); err != nil {
+		if err := db.DeleteFavorite(favorite.UserID, favorite.VideoID); err != nil {
 			return http.StatusInternalServerError, errors.New("取消点赞失败")
 		}
 
@@ -39,7 +40,7 @@ func FavoriteList(favorite *models.Favorite) ([]models.Video, error) {
 	var videoList []models.Video
 	var err error
 
-	if videoList, err = favorite.GetFavoriteListByUserID(); err != nil {
+	if videoList, err = db.GetFavoriteListByUserID(favorite.UserID); err != nil {
 		return nil, err
 	}
 
