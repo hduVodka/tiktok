9e79455baa0970a4db051930a6e8540db2b39e86
BaiMeow <baishu2003@outlook.com>
2023-01-28 23:02:41 +0800

    fix: dto json

--- dto/dto.go
+++ dto/dto.go
@@ -3,19 +3,19 @@ package dto
 type User struct {
 	Id            uint   `json:"id"`
 	Name          string `json:"name"`
-	FollowCount   uint   `json:"followCount"`
-	FollowerCount uint   `json:"followerCount"`
-	IsFollow      uint   `json:"isFollow"`
+	FollowCount   uint   `json:"follow_count"`
+	FollowerCount uint   `json:"follower_count"`
+	IsFollow      bool   `json:"is_follow"`
 }
 
 type Video struct {
 	Id            uint   `json:"id"`
 	Author        User   `json:"author"`
-	PlayUrl       string `json:"playUrl"`
-	CoverUrl      string `json:"coverUrl"`
-	FavoriteCount uint   `json:"favoriteCount"`
-	CommentCount  uint   `json:"commentCount"`
-	IsFavorite    uint   `json:"isFavorite"`
+	PlayUrl       string `json:"play_url"`
+	CoverUrl      string `json:"cover_url"`
+	FavoriteCount uint   `json:"favorite_count"`
+	CommentCount  uint   `json:"comment_count"`
+	IsFavorite    bool   `json:"is_favorite"`
 	Title         string `json:"title"`
 }
 
@@ -24,5 +24,5 @@ type Comment struct {
 	User    User   `json:"user"`
 	Content string `json:"content"`
 	//mm-dd
-	CreateDate string `json:"createDate"`
+	CreateDate string `json:"create_date"`
 }
