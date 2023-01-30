651c813490e998ed4f629357b2c05eb88965d723
BaiMeow <baishu2003@outlook.com>
2023-01-30 14:16:21 +0800

    fix: commentList has wrong response

--- controller/comment.go
+++ controller/comment.go
@@ -4,41 +4,41 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strconv"
+	"tiktok/dto"
 	"tiktok/models"
 	"tiktok/service/interactive"
 )
 
+type CommentListResp struct {
+	Resp
+	CommentList []dto.Comment `json:"comment_list"`
+}
+
 func CommentAction(c *gin.Context) {
-	var userId interface{}
+	userId := c.GetUint("userId")
 
-	userId, _ = c.Get("userId")
-
-	var actionType int
-	var videoId int
-	var content string
-	var err error
-
-	if actionType, err = strconv.Atoi(c.Query("action_type")); err != nil {
+	actionType, err := strconv.Atoi(c.Query("action_type"))
+	if err != nil {
 		c.JSON(http.StatusBadRequest, Resp{
 			400,
-			"请求参数错误",
+			ErrInvalidParams,
+		})
+		return
+	}
+	videoId, err := strconv.Atoi(c.Query("video_id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, Resp{
+			400,
+			ErrInvalidParams,
 		})
 		return
 	}
 
-	if videoId, err = strconv.Atoi(c.Query("video_id")); err != nil {
-		c.JSON(http.StatusBadRequest, Resp{
-			400,
-			"请求参数错误",
-		})
-		return
-	}
-
-	content = c.Query("content")
+	content := c.Query("content")
 
 	comment := &models.Comment{
 		VideoID: uint(videoId),
-		UserID:  userId.(uint),
+		UserID:  userId,
 		Content: content,
 	}
 	if code, err := interactive.CommentAction(comment, actionType); err != nil {
@@ -55,25 +55,26 @@ func CommentAction(c *gin.Context) {
 }
 
 func CommentList(c *gin.Context) {
-	var videoId int
-	var err error
-	if videoId, err = strconv.Atoi(c.Query("video_id")); err != nil {
+	videoId, err := strconv.ParseUint(c.Query("video_id"), 10, 32)
+	if err != nil {
 		c.JSON(http.StatusBadRequest, Resp{
 			400,
-			"请求参数错误",
+			ErrInvalidParams,
 		})
 	}
 
-	if commentList, err := interactive.CommentList(uint(videoId)); err != nil {
+	if commentList, err := interactive.CommentList(c, uint(videoId)); err != nil {
 		c.JSON(http.StatusInternalServerError, Resp{
 			500,
 			"获取评论列表失败",
 		})
 	} else {
-		c.JSON(http.StatusOK, gin.H{
-			"StatusCode":   200,
-			"StatusMsg":    "获取评论列表成功",
-			"Comment_list": commentList,
+		c.JSON(http.StatusOK, CommentListResp{
+			Resp: Resp{
+				StatusCode: 0,
+				StatusMsg:  "success",
+			},
+			CommentList: commentList,
 		})
 	}
 }
