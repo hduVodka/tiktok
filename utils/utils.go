24cf206070a8556fd011d4cf1720d7c8024cab00
BaiMeow <baishu2003@outlook.com>
2023-01-30 22:45:42 +0800

    refactor: GenerateSalt

--- utils/utils.go
+++ utils/utils.go
@@ -8,7 +8,6 @@ import (
 	"tiktok/config"
 	"tiktok/log"
 	"time"
-	"unsafe"
 )
 
 func Init() {
@@ -40,27 +39,12 @@ const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 var src = rand.NewSource(time.Now().UnixNano())
 
-const (
-	// 6 bits to represent a letter index
-	letterIdBits = 6
-	// All 1-bits as many as letterIdBits
-	letterIdMask = 1<<letterIdBits - 1
-	letterIdMax  = 63 / letterIdBits
-)
-
 func GenerateSalt() string {
 	b := make([]byte, 8)
-	// A rand.Int63() generates 63 random bits, enough for letterIdMax letters!
-	for i, cache, remain := 7, src.Int63(), letterIdMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = src.Int63(), letterIdMax
-		}
-		if idx := int(cache & letterIdMask); idx < len(letters) {
-			b[i] = letters[idx]
-			i--
-		}
-		cache >>= letterIdBits
-		remain--
+	// A rand.Int63() generates 63 random bits
+	for i, cache := 7, src.Int63(); i >= 0; i-- {
+		b[i] = letters[int(cache)%len(letters)]
+		cache >>= 6
 	}
-	return *(*string)(unsafe.Pointer(&b))
+	return string(b[:])
 }
